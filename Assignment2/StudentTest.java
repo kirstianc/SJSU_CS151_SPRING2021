375c5e517a5190a79f421e038ba66cb35a7f0220
Ian <kirstianalfred.chavez@sjsu.edu>
2021-02-23 13:02:40 -0800

    These are my chanegs to Assignment 2

---
+++ Assignment2/StudentTest.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+/** Used to test Student class
+ * @author Ian Chavez
+ */
+public class StudentTest{
+
+	// Creates a Student: John Smith, age 20, gpa 3.6, Major Computer Science, Department School of Computer Science
+	public static void main(String[] args){
+		Student j = new Student("John", "Smith", 20, 3.6f, "Computer Science", "School of Computer Science");
+		j.new Course().printSchedule();
+	}
+
+}
